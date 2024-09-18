b7788255443c47a0f695e330d354d8a9cc46a157
mamniastepcoder <tannu.mamnia@stepcoders.com>
2024-09-18 15:44:06 +0530

    Update booksearch.blade.php

--- resources/views/api/booksearch.blade.php
+++ resources/views/api/booksearch.blade.php
@@ -9,16 +9,13 @@
 <body>
     <div class="container">
         <h1 class="mt-4 text-center p-3 mb-2 bg-info text-white">Book Search</h1>
-
         <form action="{{ route('books-search') }}" method="GET" class="mb-4">
             <div class="form-group mt-4" >
                 <label><b class="text-success">Search Books</b> </label>
                 <input type="text" name="search" class="" placeholder="Search for books..." value="{{ $search }}">
                  <button type="submit" class="btn btn-primary">Search</button>
             </div>
-           
-        </form>
-
+           </form>
         @if (!empty($books))
             <table class="table table-striped">
                 <thead>
@@ -48,7 +45,6 @@
             </table>
         @endif
     </div>
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
