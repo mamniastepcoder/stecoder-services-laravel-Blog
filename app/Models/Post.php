56e57fda8b4e97880ebb0a0bb3ddb26d73e93650
mamniastepcoder <tannu.mamnia@stepcoders.com>
2024-09-18 15:50:26 +0530

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,11 +9,8 @@ class Post extends Model
 {
     use HasFactory;
   // The table associated with the model.
-   
-    protected $table = 'posts';
-    
+   protected $table = 'posts';
     // assigned field name
-
     protected $fillable = [
         'author_name',
         'title',
