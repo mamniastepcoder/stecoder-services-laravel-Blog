cc92ec321535c82b3f07829046a0e7b91e0710e8
mamniastepcoder <tannu.mamnia@stepcoders.com>
2024-09-18 15:38:16 +0530

    Update edit_post.blade.php

--- resources/views/edit_post.blade.php
+++ resources/views/edit_post.blade.php
@@ -5,9 +5,7 @@
      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-  
-
-     <div class="container w-50">
+    <div class="container w-50">
         <h1 class="mt-4  text-center p-3 mb-2 bg-info text-white">Edit Post</h1>
         <form action="{{ route('posts.update', $post->id) }}" class="border border-warning mt-5 p-4"  method="POST"  >
             @csrf
@@ -18,8 +16,7 @@
             <div class="text-danger">{{$message}}</div>
             @enderror
             </div>
-          
-            <div class="form-group">
+          <div class="form-group">
                 <label for="title"><b class="text-info">Title</b></label>
                 <input type="text" class="form-control" value="{{ $post->title }}" name="title" >
                    @error('title')
@@ -33,7 +30,6 @@
             <div class="text-danger">{{$message}}</div>
             @enderror
             </div>
-           
             <button type="submit" class="btn btn-success">Update</button>
              <a href="{{ route('posts') }}" class="btn btn-secondary">Back to Posts</a>
         </form>
